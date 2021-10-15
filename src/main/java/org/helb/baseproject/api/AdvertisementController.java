d7e3333b6cbbce9b124763aa1896dd77cd030f1b
yseddiki <78831250+yseddiki@users.noreply.github.com>
2021-10-15 14:48:06 +0200

    Create a Random Ad Function

--- src/main/java/org/helb/baseproject/api/AdvertisementController.java
+++ src/main/java/org/helb/baseproject/api/AdvertisementController.java
@@ -10,15 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ad")
 public class AdvertisementController {
 
     @Autowired
     private AdvertissementService advertissementService;
-
-    @GetMapping
+        @GetMapping("api/ad/getAllAd")
     public List<Advertisement> getAllAd (){
         return this.advertissementService.getAllAd();
+    }
+    @GetMapping("api/ad/getSizeAdAPI")
+    public Long  getSizeAdAPI() {
+        return  this.advertissementService.getSizeAdApi();
+    }
+    @GetMapping("api/ad/getRandAd")
+    public Advertisement  getRandAd() {
+        return  this.advertissementService.getRandAd();
     }
 
 }
--- src/main/java/org/helb/baseproject/model/dto/api/AdvertissementService.java
+++ src/main/java/org/helb/baseproject/model/dto/api/AdvertissementService.java
@@ -3,9 +3,12 @@ package org.helb.baseproject.model.dto.api;
 import org.helb.baseproject.model.entity.Advertisement;
 import org.helb.baseproject.repo.AdvertissementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class AdvertissementService {
@@ -16,5 +19,12 @@ public class AdvertissementService {
     public List<Advertisement> getAllAd (){
         return this.advertissementRepository.findAll();
     }
-
+    public long getSizeAdApi () {
+        return advertissementRepository.count();
+    }
+    public Advertisement getRandAd () {
+        List<Advertisement> ads = advertissementRepository.findAll();
+        int randAd = (int)(Math.random()*ads.size()+1);
+        return ads.get(randAd);
+    }
 }
